06d59702e5f794efc63ba03ce2cce80383342429
Ramazan Umutcan Besli <ramazanumutcanbesli@hotmail.com>
2022-12-20 17:42:39 +0100

    First Test Scenario yazıldı. Bu senaryoda çıkan süreli hata mesajı tes edildi

--- playwright.config.js
+++ playwright.config.js
@@ -4,14 +4,16 @@ const { devices } = require('@playwright/test');
 const config = {
   testDir: './tests',
   /* Maximum time one test can run for. */
-  timeout: 30 * 1000,
-  expect: {
+  timeout: 30 * 1000, // test stepleri için beklenen max süre
+  expect: { // assertion yaparken beklenen süreyi yazdık
     timeout: 5000
   },
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    
+    // We have to tell which properties that we have f.example: browser type
+    browserName: 'chromium',
+    headless: false
   },
 
 };
--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -8,16 +8,37 @@
 
 
 
-const {test} = require('@playwright/test');
-test('Browser Context Decleration Playwright test',async ({browser})=>
+const {test, expect} = require('@playwright/test');
+test.only('Browser Context Decleration Playwright test',async ({browser})=>
 {
     // We write our code inside this function
     const context = await browser.newContext();
     const page = await context.newPage();
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    console.log(await page.title());
+    await expect(page).toHaveTitle('LoginPage Practise | Rahul Shetty Academy');
+    await page.locator('input#username').type("practice");
+    await page.locator("[name='password']").type("practice");
+    await page.locator('input#terms').click();
+    await page.locator('input#signInBtn').click();
+    /**
+    * aşagıda yazılan kod satırında otomasyon edilirken yanlış şifre ve kullanıcı adından kaynaklı
+    * bir hata mesajı sergilemekte inspect edip bakıldıgında style attribute name none dan blocka
+    * dönüşmekte. Ve saniyeler içerisinde hata mesajı yok olmakta bu yüzden * işareti kullanılarak
+    * bu web elementin bir parçası sadece alındı. sonrasında textContent fonksiyonu ile bu tag 
+    * içerisinde yazılı olan string alınmış oldu 
+    */
+
+    console.log(await page.locator("[style*='block']").textContent());
+
+    // toContainText() alınan textin bir parçasında olup olmadıgının kontrolunu yapar.
+    await expect(page.locator("[style*='block']")).toContainText('username/password.');
 });
 
 test('Page decleration Playwright Test',async ({page})=>
 {
-    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    await page.goto("https://www.google.com");
+    console.log(await page.title());
+    await expect(page).toHaveTitle('Google');
+
 });
